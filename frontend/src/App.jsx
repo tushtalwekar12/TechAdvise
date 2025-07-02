19634b127e2dbdbb45f83168cf4e36003cfac056
Tushar Talwekar <tushartalwekar1234@gmail.com>
2025-07-02 17:23:28 +0530

    hero section work done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 // frontend/src/App.jsx
 import React from 'react';
 import Header from '../common/Header';
+import Hero from '../components/HeroSection';
 import './App.css';
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-white" style={{ fontFamily: '"Work Sans", "Noto Sans", sans-serif' }}>
       <Header />
+      <Hero />
       {/* You can add other sections like Hero, Footer etc. here */}
     </div>
   );
