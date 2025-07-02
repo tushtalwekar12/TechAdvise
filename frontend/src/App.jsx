2840f77772be53b0c26d1f2560fbdb006a96af4c
sammy171207 <sameerrandive171207@gmail.com>
2025-07-02 20:31:26 +0530

    a

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@
 import React from 'react';
 import Header from '../common/Header';
 import './App.css';
+import HomePage from '../pages/HomePage';
 
 function App({ children }) {
   return (
     <div className="min-h-screen flex flex-col bg-white" style={{ fontFamily: '"Work Sans", "Noto Sans", sans-serif' }}>
       <Header />
+      
       {/* You can add other sections like Hero, Footer etc. here */}
     </div>
   );
