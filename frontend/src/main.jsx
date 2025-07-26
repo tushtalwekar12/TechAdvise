bbd71b73ecfa76abc648e2b4d85dd71b6905ec75
sammy171207 <sameerrandive171207@gmail.com>
2025-07-26 20:05:39 +0530

    check

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,13 +5,16 @@ import './index.css'
 import { Provider } from 'react-redux';
 import store from './app/store';
 import { BrowserRouter } from 'react-router-dom';
-import AppRoutes from './routes/AppRoutes';
+import AppRoutes from './routes/AppRoutes.jsx';
+import { HelmetProvider } from 'react-helmet-async';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <Provider store={store}>
-     <BrowserRouter>
-       <AppRoutes />
-     </BrowserRouter>
-   </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </Provider>
+    </HelmetProvider>
   </StrictMode>,
 )
