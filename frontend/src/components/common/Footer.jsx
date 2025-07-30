7bf3c9e82d85f94d9b8c09aca095f88162d8e6dc
Tushar Talwekar <tushartalwekar1234@gmail.com>
2025-07-30 13:48:33 +0530

    updated

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -49,7 +49,7 @@ const Footer = () => {
           </a>
 
           <a
-            href="https://instagram.com/yourcompany"
+            href="https://www.instagram.com/techadvise.in?igsh=MW0wYzVkNGxqY2Nveg=="
             target="_blank"
             rel="noopener noreferrer"
             className="text-[#60768a] hover:text-blue-600"
@@ -61,7 +61,7 @@ const Footer = () => {
           </a>
 
           <a
-            href="https://linkedin.com/company/yourcompany"
+            href="http://www.linkedin.com/in/techadvise"
             target="_blank"
             rel="noopener noreferrer"
             className="text-[#60768a] hover:text-blue-600"
